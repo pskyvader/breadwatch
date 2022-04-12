2372c076716a9ef3676e04aaee0aef3856b7a814
Pablo <pskyvader@hotmail.com>
2022-04-11 19:50:22 -0500

    a

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { initializeIcons } from "@fluentui/font-icons-mdl2";
+import "office-ui-fabric-core";
 
 initializeIcons();
 // After
