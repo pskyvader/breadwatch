4eb272fd42f1138b03c9d038d755f5b538fcd90a
Pablo <pskyvader@hotmail.com>
2022-05-15 01:12:56 -0500

    --- Auto-Git Commit ---

--- client/src/components/Statistics.jsx
+++ client/src/components/Statistics.jsx
@@ -6,12 +6,13 @@ import { useState, useEffect } from "react";
 
 import { getAllLogs } from "../API/logs";
 
-
-const parseData = (data,field) => {
+const parseData = (data, field) => {
 	const dataArray = data.map((element) => {
 		return {
 			x: new Date(element.date),
 			y: element[field],
+			xAxisCalloutData: element.date,
+			yAxisCalloutData: element[field],
 		};
 	});
 	console.log("dataArray", dataArray);
@@ -40,9 +41,9 @@ const Statistics = () => {
 			</div>
 		);
 	}
-	const data1 = parseData(dataset,"bread");
-	const data2 = parseData(dataset,"cookie");
-	const data3 = parseData(dataset,"cake");
+	const data1 = parseData(dataset, "bread");
+	const data2 = parseData(dataset, "cookie");
+	const data3 = parseData(dataset, "cake");
 
 	const data4 = {
 		chartTitle: "Line Chart",
@@ -64,37 +65,33 @@ const Statistics = () => {
 			},
 		],
 	};
+	const margins = { left: 35, top: 20, bottom: 35, right: 20 };
 
 	return (
 		<div>
-			<Stack
+			{/* <Stack
 				style={{
 					padding: DefaultSpacing.l2,
 					boxShadow: Depths.depth4,
 					background: NeutralColors.white,
 				}}
-			>
+			> */}
 				<LineChart
-            culture={window.navigator.language}
-            data={data4}
-            legendsOverflowText={'Overflow Items'}
-            yMinValue={0}
-            yMaxValue={20}
-			height={600}
-			width={600}
-            // margins={margins}
-            allowMultipleShapesForPoints={true}
-          />
-				{/* <LineChart
 					data={data4}
-					strokeWidth={4}
-					tickFormat={"%m/%d"}
+					legendsOverflowText={"Overflow Items"}
+					width={300}
 					height={600}
-					width={600}
-					hideLegend={false}
-					showXAxisLablesTooltip
-				/> */}
-			</Stack>
+					tickFormat={"%m/%d"}
+					// margins={margins}
+					// showXAxisLablesTooltip
+					// allowMultipleShapesForPoints={true}
+					// xAxisTickCount={10}
+					// legendProps={{
+					// 	canSelectMultipleLegends: true,
+					// 	allowFocusOnLegends: true,
+					// }}
+				/>
+			{/* </Stack> */}
 		</div>
 	);
 };
