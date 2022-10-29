af7c98fc21da74367645c44992dc127ad328df04
Pablo <pskyvader@hotmail.com>
2022-10-29 14:12:59 -0500

    --- Auto Git Commit ---

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize, Model } = require("sequelize");
 const { LogsConfiguration } = require("./logs");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL + "?sslmode=require", {
 	// dialect: 'postgres',
 	// protocol: 'postgres',
 	dialectOptions: {
